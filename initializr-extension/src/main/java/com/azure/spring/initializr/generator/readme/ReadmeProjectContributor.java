1403140ef0d4f42a845f7f3b78277b03e2096a46
Rujun Chen <rujche@microsoft.com>
2022-12-06 16:09:02 +0800

    Adding a workflow status badge for "Java CI with Maven workflow".

--- initializr-extension/src/main/java/com/azure/spring/initializr/generator/readme/ReadmeProjectContributor.java
+++ initializr-extension/src/main/java/com/azure/spring/initializr/generator/readme/ReadmeProjectContributor.java
@@ -1,15 +1,10 @@
 package com.azure.spring.initializr.generator.readme;
 
-import io.spring.initializr.generator.project.contributor.ProjectContributor;
+import io.spring.initializr.generator.project.contributor.MultipleResourcesProjectContributor;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+public class ReadmeProjectContributor extends MultipleResourcesProjectContributor {
 
-public class ReadmeProjectContributor implements ProjectContributor {
-    @Override
-    public void contribute(Path projectRoot) throws IOException {
-        Path file = Files.createFile(projectRoot.resolve("README.md"));
-        Files.writeString(file, "Hello World.");
+    public ReadmeProjectContributor() {
+        super("classpath:readme");
     }
 }
---
+++ initializr-extension/src/main/resources/readme/README.md
@@ -0,0 +1,4 @@
+
+# Azure Spring Initializr Project
+
+[![Java CI with Maven workflow](../../actions/workflows/maven.yml/badge.svg)](../../actions/workflows/maven.yml)
